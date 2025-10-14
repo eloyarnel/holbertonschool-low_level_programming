5eb7ad7058126d482b852e959fb01079eeb86a7e
eloyarnel <eloyaliceasanchez@gmail.com>
2025-10-14 15:29:14 -0400

    Project C-Hello, World, Task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,5 @@
-#include <studio.h>h
-int main() {
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+#include <stdio.h>
+int main(void) {
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
