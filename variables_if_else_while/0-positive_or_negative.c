08710b2b74976a6561b1e2c0f6dde8f53ba5788b
eloyarnel <eloyaliceasanchez@gmail.com>
2025-10-15 12:07:48 -0400

    Project C for Holberton Task 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -15,11 +15,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-		printf("is positive\n");
+		printf("is positive\n", n);
 	else if (n == 0)
-		printf("is zero\n");
+		printf("is zero\n", n);
 	else
-		printf("is negative\n");
+		printf("is negative\n", n);
 
 	return (0);
 }
