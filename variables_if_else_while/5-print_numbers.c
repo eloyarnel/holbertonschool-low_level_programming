1dd3c2eea5e61d60f6ca1c2f21bea8d690e47bfd
eloyarnel <eloyaliceasanchez@gmail.com>
2025-10-16 10:43:45 -0400

    Project C for Holberton Task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/** 
+ * main - Prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	char number;
+
+	for (number == '0' ; number <= '10'; number++)
+	putchar(number)
+		putchar('\n');
+	return(0);
+}
