70f68eb554fa0626d1e53dad6a4b5dd4814ea9f2
eloyarnel <eloyaliceasanchez@gmail.com>
2025-10-17 15:46:22 -0400

    Project C for Holberton Task 0

---
+++ 0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Write a program that prints _putchar followed by a lew line
+ *
+ * return: Always 0 (success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+}
