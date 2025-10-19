0634d68a7b1f7bff27e05fdb4bd1668073f44506
eloyarnel <eloyaliceasanchez@gmail.com>
2025-10-18 23:51:03 -0400

    Project C for Holberton Task 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,17 @@
  * Description: This function uses _putchar to print all
  * lowercase letters from 'a' to 'z', then prints a newline.
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
-	void print_alphabet(char c){
-	for (int i = 0; i < 10; i++) {
-		_putchar(c);
-	}
-	_putchar('\n');
-	}
-	int main() {
-		print_char_ten_times_('x');
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
 	}
 }
