c5af338df51359c746a61efb0dfdf4c521fc81c2
eloyarnel <eloyaliceasanchez@gmail.com>
2025-10-19 11:16:17 -0400

    Project C for Holberton Task 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints the alphabet in lowercase, followed by a new line.
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10 times.
  *
- * Description: This function uses _putchar to print all
- * lowercase letters from 'a' to 'z', then prints a newline.
+ * Description: This function uses a nested loop to print
+ * the lowercase alphabet followed by a new line, ten times.
+ * Return: void
  */
 void print_alphabet_x10(void)
 {
