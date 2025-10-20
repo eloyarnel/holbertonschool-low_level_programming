6aefc8d0c633a223262be0db282bdc7bc8ee80d0
eloyarnel <eloyaliceasanchez@gmail.com>
2025-10-20 09:42:40 -0400

    Project C for Holberton Task 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - A function that checks for alphabetic character.
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int _isalpha (int c)
+{
+	if ((c => 'a' ; _isalpha c <= 'z'|| c => 'A' ; _isalpha c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
