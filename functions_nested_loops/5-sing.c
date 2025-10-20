0ae482b2b9219d288975992d2726100935570743
eloyarnel <eloyaliceasanchez@gmail.com>
2025-10-20 10:32:51 -0400

    Project C for Holberton Task 4

---
+++ functions_nested_loops/5-sing.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - write a function that prints the sing of a number
+ *
+ * Return - Always 0
+ */
+int print_sign(int n);
+{
+	if (n >= '1')
+		_putchar('+');
+	else if (n == 0)
+		_putchar('0');
+	else (n <= '1')
+		_putchar('-');
+	return (0);
+}
