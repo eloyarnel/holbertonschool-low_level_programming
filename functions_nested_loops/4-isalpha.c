b37b66664210d58c84c6808273f9a267293c686d
eloyarnel <eloyaliceasanchez@gmail.com>
2025-10-20 09:54:13 -0400

    Project C for Holberton Task 4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -7,7 +7,7 @@
  */
 int _isalpha (int c)
 {
-	if ((c => 'a' ; _isalpha c <= 'z'|| c => 'A' ; _isalpha c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
