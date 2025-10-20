10744f7dfd184e42f2a59a64ff6496317d6dbfab
eloyarnel <eloyaliceasanchez@gmail.com>
2025-10-20 11:54:00 -0400

    Project C for Holberton Task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _abs -Computes the absolute value of an integer
+ * @r: The integer to be converted to absolute value
+ *
+ * Return: The absolute value of r
+ */
+int abs(int r);
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return (1);
+	}
+	else if (n)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
