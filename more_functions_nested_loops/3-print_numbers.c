9b0b1bb9a8fe645264245723e6b35f691fada470
eloyarnel <eloyaliceasanchez@gmail.com>
2025-10-21 13:06:25 -0400

    Project C for Holberton Task 3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,17 @@
 #include "main.h"
+
 /**
+ * print_numbers - Prints the numbers from 0 to 9
  *
- *
- *
+ * Return: Nothing
  */
-int print_numbers(void)
+void print_numbers(void)
 {
+	char number;
+
 	for (number = '0'; number <= '9'; number++)
+	{
 		_putchar(number);
-
+	}
 	_putchar('\n');
-
-	return (0);
 }
