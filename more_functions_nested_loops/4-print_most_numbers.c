09dc7102aa35f1957fc5f1745197d67763e0907a
eloyarnel <eloyaliceasanchez@gmail.com>
2025-10-21 18:33:21 -0400

    Project C for Holberton Task 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Prints the number from 0 to 9, followed by a new line 
+ *
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int void print_most_numbers(void)
+{
+	char number;
+
+	for (number = '0'; number != 4 ; number != 2 ; number <= '9';)
+		_putchar(number);
+	_putchar('\n');
+
+	return (0);
+}
