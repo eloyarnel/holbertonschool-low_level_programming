882c11f56072b317b842b62333dbab612973225f
eloyarnel <eloyaliceasanchez@gmail.com>
2025-10-21 12:22:34 -0400

    Project C for Holberton Task 1

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * int mul - a function that multiplies two intergers
+ * @a @b: The character check
+ *
+ *Return: Always 0
+ */
+int mul(int a, int b)
+{
+	if (a >= '0' && b <= '9')
+		return (1);
+	else
+		return (0);
+}
