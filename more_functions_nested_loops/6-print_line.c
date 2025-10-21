1617cac6000a2eaf7299fc1db76692f07ec4a589
eloyarnel <eloyaliceasanchez@gmail.com>
2025-10-21 19:29:02 -0400

    Project C for Holberton Task 6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -4,13 +4,13 @@
  *
  * Return: Nothing
  */
-void print_line_line(int n)
+void print_line(int n)
 {
-	int line;
+	int lin;
 
 	if (n > 0)
 	{
-		for (line = 0; line < n; line++)
+		for (lin = 0; lin < n; lin++)
 			_putchar('_');
 	}
 	_putchar('\n');
