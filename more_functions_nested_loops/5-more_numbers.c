b38d3a7c9136e6c595be9d7e100a750ba64ee177
eloyarnel <eloyaliceasanchez@gmail.com>
2025-10-21 18:59:43 -0400

    Project C for Holberton Task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - write a function that prints 10 times the numbers, from 0 to 14, followed by a new line
+ *
+ * Return: Nothing
+ */
+void more_numbers(void);
+{
+	int numb, count;
+
+	for (number = '0'; number <= '14'; number++)
+	{
+		_putchar((numb / 10) + '0');
+	_putchar((numb % 10) + '0');
+	}
+	_putchar('\n');
+}
