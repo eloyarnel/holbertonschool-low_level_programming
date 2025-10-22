19acbdc071ccad8866d3bec0f9de6cfcde199cb8
eloyarnel <eloyaliceasanchez@gmail.com>
2025-10-21 20:56:43 -0400

    Project C for Holberton Task 8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,8 +1,10 @@
 #include "main.h"
 /**
- * main - Write a function that prints a aquare, followed by a new line
- * @size: The size of the squeare.
+ * print_square - Prints a square using the character '#'
+ * @size: The size of the square
  *
+ * Description: If size is 0 or less, the function prints only a new line.
+ * Return: void
  */
 void print_square(int size)
 {
