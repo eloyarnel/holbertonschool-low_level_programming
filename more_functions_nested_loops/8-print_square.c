3a7afdfe61ef90f9941a3156aa79ad2f30d30ad4
eloyarnel <eloyaliceasanchez@gmail.com>
2025-10-21 20:52:01 -0400

    Project C for Holberton Task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * main - Write a function that prints a aquare, followed by a new line
+ * @size: The size of the squeare. 
+ *
+ */
+void print_square(int size)
+{
+	int high, wid;
+
+	if (size > 0)
+	{
+		for (high = 0; high < size; high++)
+		{
+			for (wid = 0; wid < size; wid++)
+				_putchar('#');
+
+			if (high == size - 1)
+				continue;
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}
