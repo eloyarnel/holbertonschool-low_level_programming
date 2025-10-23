fb5307cf0e5b30def814999c2e204c1777a56165
eloyarnel <eloyaliceasanchez@gmail.com>
2025-10-23 13:27:10 -0400

    Project for C Lenguage for Holberton Task 1

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
- * reset_to_98 - takes a pointer to an int and updates the value it points to 98
- * * @n: pointer to an integer
- *
+ * reset_to_98 - updates the value of a pointer to 98
+ * @ n: pointer to an interger
  * Return: void
  */
 void reset_to_98(int *n)
 {
-	*n = 9+8;
+ *n = 98;
 }
