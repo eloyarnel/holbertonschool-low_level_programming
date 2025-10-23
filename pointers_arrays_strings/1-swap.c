8ec5d5b798d1baf4a79272b4026c134aeb47f1d1
eloyarnel <eloyaliceasanchez@gmail.com>
2025-10-23 14:53:24 -0400

    Project for C Lenguage for Holberton Task 1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /** 
  * swap_int - swaps the values of two integers
- * @ a: pointer to first integer
+ * @a : pointer to first integer
  * Return: void
- * @ b: pointer to second integer
+ * @b: pointer to second integer
  */ 
 void swap_int(int *a, int *b)
 {
