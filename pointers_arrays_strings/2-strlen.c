33f0221f67e882856ec1a0ad57721dc8785b9b8a
eloyarnel <eloyaliceasanchez@gmail.com>
2025-10-25 19:26:54 -0400

    fix: DOS line ending fix

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,19 @@
 #include "main.h"
 /**
  * _strlen - returns the length of a string
- * 
- * 
+ *
+ * @s: string to evaluate
+ *
+ * Return: length of the string
+ *
  */
 int _strlen(char *s)
 {
-int len = 0;
-while (s[len] != '\0')
-{
-len++;
-}
-return (len);
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
 }
