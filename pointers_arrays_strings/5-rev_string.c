19e3a8956fed1b4863c804f92b55f1a0561882a8
eloyarnel <eloyaliceasanchez@gmail.com>
2025-10-25 19:30:19 -0400

    fix: DOS line ending betty error

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,24 +1,25 @@
 #include "main.h"
 /**
- * reset_to_98 - updates the value of a pointer to 98
- * @ n: pointer to an interger
+ * rev_string - reverses a string
+ * @s: string to reverse
+ *
  * Return: void
  */
 void rev_string(char *s)
 {
-    int len = 0, i;
-    char temp;
+	int len = 0, i;
+	char temp;
 
-    while (s[len] != '\0')
-    {
-        len++;
-    }
-    len--;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	len--;
 
-    for (i = 0; i < len; i++, len--)
-    {
-        temp = s[i];
-        s[i] = s[len];
-        s[len] = temp;
-    }
+	for (i = 0; i < len; i++, len--)
+	{
+		temp = s[i];
+		s[i] = s[len];
+		s[len] = temp;
+	}
 }
