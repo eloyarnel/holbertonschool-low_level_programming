2dacc018d35e514da6d8e8dc20ed45c89522f9a8
eloyarnel <eloyaliceasanchez@gmail.com>
2025-10-27 13:30:49 -0400

    Project for C Lenguage for Holberton Task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: pointer to the first element of the array
+ * @n: number of elements to print
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        if (i != 0)
+            _putchar(',');
+        _putchar(' ');
+        if (a[i] < 0)
+        {
+            _putchar('-');
+            print_number(-a[i]);
+        }
+        else
+        {
+            print_number(a[i]);
+        }
+    }
+    _putchar('\n');
+}
