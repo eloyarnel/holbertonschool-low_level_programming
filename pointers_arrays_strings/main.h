ef0d44aba4330ad17349775f1b11aaed00e2c421
eloyarnel <eloyaliceasanchez@gmail.com>
2025-10-29 14:06:46 -0400

    Project for C Lenguage for Holberton Task 2

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -26,4 +26,5 @@ char *leet(char *);
 /** Prototype from: Even more pointers, arrays and strings */
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 #endif /*MAIN_H*/
