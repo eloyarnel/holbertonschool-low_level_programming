a9ba2da50d45b182d8f4c17a15ee6c5746ee7bf9
eloyarnel <eloyaliceasanchez@gmail.com>
2025-10-29 12:09:29 -0400

    Project for C Lenguage for Holberton Task 0

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -23,4 +23,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+/** Prototype from: Even more pointers, arrays and strings */
+char *_memset(char *s, char b, unsigned int n);
 #endif /*MAIN_H*/
