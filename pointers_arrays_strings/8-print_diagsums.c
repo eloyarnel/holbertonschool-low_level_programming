7a540fcbfc59d90963fcf869b1157a2213f8122b
eloyarnel <eloyaliceasanchez@gmail.com>
2025-10-30 14:44:35 -0400

    Project for C Lenguage for Holberton Task 8

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_diagsums - prints the sum of the two diagonals of a square matrix
  * @a: pointer to the first element of the matrix
@@ -9,15 +10,12 @@ void print_diagsums(int *a, int size)
 int i;
 int sum1 = 0;
 int sum2 = 0;
+
 for (i = 0; i < size; i++)
 {
-sum1 += *(a + i * size + i);           /* Primary diagonal */
-sum2 += *(a + i * size + (size - 1 - i)); /* Secondary diagonal */
+sum1 += *(a + i * size + i);
+sum2 += *(a + i * size + (size - 1 - i));
 }
-_putchar(sum1 / 10 + '0'); /* Print tens digit of sum1 */
-_putchar(sum1 % 10 + '0'); /* Print units digit of sum1 */
-_putchar('\n');
-_putchar(sum2 / 10 + '0'); /* Print tens digit of sum2 */
-_putchar(sum2 % 10 + '0'); /* Print units digit of sum2 */
-_putchar('\n');
+
+printf("%d, %d\n", sum1, sum2);
 }
