8f6ac8bd795f3ddf879336bf3689a598a70e991a
eloyarnel <eloyaliceasanchez@gmail.com>
2025-10-30 12:02:41 -0400

    Project for C Lenguage for Holberton Task 5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: string to search in
+ * @needle: substring to search for
+ * Return: pointer to the beginning of the located substring,
+ * or NULL if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+char *h;
+char *n;
+
+if (*needle == '\0')
+return (haystack);
+
+while (*haystack != '\0')
+{
+h = haystack;
+n = needle;
+
+while (*haystack != '\0' && *n != '\0' && *haystack == *n)
+{
+haystack++;
+n++;
+}
+
+if (*n == '\0')
+return (h);
+
+haystack = h + 1;
+}
+
+return (0);
+}
