1bbfd726363ad11b0954a3095b6a15bd82e40d6b
eloyarnel <eloyaliceasanchez@gmail.com>
2025-11-03 13:51:54 -0400

    Project for C Lenguage for Holberton Task 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -5,11 +5,11 @@
  * @guess: current guess for the square root
  * Return: the square root of n or -1 if not a perfect square
  */
-int sqrt_helper(int n, int guess)
+int sqrt_recursion_helper(int n, int guess)
 {
 if (guess * guess == n)
 return (guess);
 if (guess * guess > n)
 return (-1);
-return (sqrt_helper(n, guess + 1));
+return (sqrt_recursion_helper(n, guess + 1));
 }
