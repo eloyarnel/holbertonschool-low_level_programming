5b357b46081b60babb7ee538e8efb11c41b997e3
eloyarnel <eloyaliceasanchez@gmail.com>
2025-11-03 13:58:17 -0400

    Project for C Lenguage for Holberton Task 6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: number to check
+ * Return: 1 if prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+if (n <= 1)
+return 0;
+return is_prime_helper(n, 2);
+}
