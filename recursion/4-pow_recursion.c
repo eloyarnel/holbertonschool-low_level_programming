19525b65cf582de01874d26edfe7a7f4eeb69fc3
eloyarnel <eloyaliceasanchez@gmail.com>
2025-11-03 12:22:52 -0400

    Project for C Lenguage for Holberton Task 4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion - function that returns the value x raised to the y
+ * @x: base number
+ * @y: exponent number
+ * Return: value of x raised to the power of y, -1 if y < 0
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+if (y == 0)
+return (1);
+return (x * _pow_recursion(x, y - 1));
+}
