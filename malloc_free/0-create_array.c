77aef964a5ec270e5386f172a88efc72a7346589
eloyarnel <eloyaliceasanchez@gmail.com>
2025-11-04 11:16:15 -0400

    Project for C Lenguage for Holberton Task 0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *create_array - creates array of chars, and initializes with a specific char
+ * @size: size of the array
+ * @c: character to initialize the array with
+ * Return: pointer to the array, or NULL if it fails
+ */
+char *create_array(unsigned int size, char c);
+{
+char *array;
+unsigned int i;
+
+if (size == 0)
+return (NULL);
+
+array = (char *)malloc(size * sizeof(char));
+if (array == NULL)
+return (NULL);
+
+for (i = 0; i < size; i++)
+{
+array[i] = c;
+}
+
+return (array);
+}
