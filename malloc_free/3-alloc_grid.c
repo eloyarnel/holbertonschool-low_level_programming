1a54954f100540d7603abe0eedf5c5f03f680d24
eloyarnel <eloyaliceasanchez@gmail.com>
2025-11-04 14:37:17 -0400

    Project for C Lenguage for Holberton Task 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * alloc_grid - function that returns a pointer to a 2 dimensional array of integers
+ * **alloc_grid - function that returns a pointer to a 2 dimensional array of integers
  * @width: width of the grid
  * @height: height of the grid
  * Return: pointer to the 2D array, or NULL on failure
