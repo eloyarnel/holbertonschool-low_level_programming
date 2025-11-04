272cabd428c614aa89dfcb41670bba4f0e21b375
eloyarnel <eloyaliceasanchez@gmail.com>
2025-11-04 13:35:53 -0400

    Project for C Lenguage for Holberton Task 1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -8,17 +8,17 @@
  */
 char *_strdup(char *str)
 {
-    char *dup;
-    int i, len = 0;
-    if (str == NULL)
-        return (NULL);
-    while (str[len] != '\0')
-        len++;
-    dup = malloc((len + 1) * sizeof(char));
-    if (dup == NULL)
-        return (NULL);
-    for (i = 0; i < len; i++)
-        dup[i] = str[i];
-    dup[len] = '\0';
-    return (dup);
+char *dup;
+int i, len = 0;
+if (str == NULL)
+return (NULL);
+while (str[len] != '\0')
+len++;
+dup = malloc((len + 1) * sizeof(char));
+if (dup == NULL)
+return (NULL);
+for (i = 0; i < len; i++)
+dup[i] = str[i];
+dup[len] = '\0';
+return (dup);
 }
