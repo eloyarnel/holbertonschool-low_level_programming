c8ffd3b071a0ae532ecf15cf7e18c22f91f85449
eloyarnel <eloyaliceasanchez@gmail.com>
2025-11-04 14:33:41 -0400

    Project for C Lenguage for Holberton Task 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -6,31 +6,32 @@
  * @height: height of the grid
  * Return: pointer to the 2D array, or NULL on failure
  */
-int **alloc_grid(int width, int beight)
+int **alloc_grid(int width, int height)
 {
-    int **grid;
-    int i, j;
+	int **grid;
+	int i, j;
 
-    if (width <= 0 || height <= 0)
-        return (NULL);
+	if (width <= 0 || height <= 0)
+		return (NULL);
 
-    grid = malloc(height * sizeof(int *));
-    if (grid == NULL)
-        return (NULL);
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
 
-    for (i = 0; i < height; i++)
-    {
-        grid[i] = malloc(width * sizeof(int));
-        if (grid[i] == NULL)
-        {
-            for (j = 0; j < i; j++)
-                free(grid[j]);
-            free(grid);
-            return (NULL);
-        }
-        for (j = 0; j < width; j++)
-            grid[i][j] = 0;
-    }
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			while (i--)
+				free(grid[i]);
+			free(grid);
+			return (NULL);
+		}
 
-    return (grid);
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+
+	return (grid);
 }
