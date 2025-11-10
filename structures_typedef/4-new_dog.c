61e2b1a18e4421939fedc977485c3203a68cbb8c
eloyarnel <eloyaliceasanchez@gmail.com>
2025-11-10 16:09:00 -0400

    Prototype for this project Task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,7 +9,43 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *newDog;
-    char *nameCopy, *ownerCopy;
-    int i, nameLen = 0, ownerLen = 0;
+dog_t *newDog;
+unsigned int i, nameLen = 0, ownerLen = 0;
+
+if (name == NULL || owner == NULL)
+return (NULL);
+
+while (name[nameLen])
+nameLen++;
+while (owner[ownerLen])
+ownerLen++;
+
+newDog = malloc(sizeof(dog_t));
+if (newDog == NULL)
+return (NULL);
+
+newDog->name = malloc(nameLen + 1);
+if (newDog->name == NULL)
+{
+free(newDog);
+return (NULL);
+}
+
+newDog->owner = malloc(ownerLen + 1);
+if (newDog->owner == NULL)
+{
+free(newDog->name);
+free(newDog);
+return (NULL);
+}
+
+for (i = 0; i <= nameLen; i++)
+newDog->name[i] = name[i];
+
+for (i = 0; i <= ownerLen; i++)
+newDog->owner[i] = owner[i];
+
+newDog->age = age;
+
+return (newDog);
 }
