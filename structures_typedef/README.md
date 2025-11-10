4b4050b182cef02d49eab9bfc2d39b261236dae4
eloyarnel <eloyaliceasanchez@gmail.com>
2025-11-10 12:55:23 -0400

    Project for typedef Task 0

---
+++ structures_typedef/README.md
@@ -0,0 +1,1 @@
+Read me for Project of Stucture and typedef in C lenguage for Holberton.
---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * struct dog - structure for dog information
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+
+dog.name = "Max";
+dog.age = 5.0;
+dog.owner = "Eloy";
+return (0);
+}
