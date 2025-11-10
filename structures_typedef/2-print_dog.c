ab09039493faf4af3f635e15fed4816dcce19339
eloyarnel <eloyaliceasanchez@gmail.com>
2025-11-10 15:46:10 -0400

    Prototype for this project Task 2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - prints a struct dog
+ * @d: pointer to struct dog
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+{
+printf("Dog is NULL\n");
+return;
+}
+}
