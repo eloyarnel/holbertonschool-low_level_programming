6506cc3d8482f084d5f87b1b8ff23a8447714c86
eloyarnel <eloyaliceasanchez@gmail.com>
2025-11-10 16:03:18 -0400

    Prototype for this project Task 4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * new_dog - creates a new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Return: pointer to the new dog (Success), NULL (Fail)
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+    dog_t *newDog;
+    char *nameCopy, *ownerCopy;
+    int i, nameLen = 0, ownerLen = 0;
+}
