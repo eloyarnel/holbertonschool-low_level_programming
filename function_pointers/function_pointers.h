56c48eeb4c46a7afc33678618ca6e3b46bdfa8a6
eloyarnel <eloyaliceasanchez@gmail.com>
2025-11-11 11:57:22 -0400

    Project for Function Pointers Task 1

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,5 +1,7 @@
-#ifndef function_pointers_h
-#define function_pointers_h
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+#include <stddef.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 #endif /* function_pointers.h */
