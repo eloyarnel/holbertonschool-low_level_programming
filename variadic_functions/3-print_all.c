f7ef5277e44fcaeba96833cd2f97a01818c5daae
eloyarnel <eloyaliceasanchez@gmail.com>
2025-11-11 15:12:38 -0400

    Project for Variadic Functions Task 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -8,39 +8,39 @@
  */
 void print_all(const char * const format, ...)
 {
-    va_list args;
-    unsigned int i = 0, printed = 0;
-    char *str;
+va_list args;
+unsigned int i = 0, printed = 0;
+char *str;
 
-    va_start(args, format);
+va_start(args, format);
 
-    while (format && format[i])
-    {
-        switch (format[i])
-        {
-            case 'c':
-                printf("%s%c", printed ? ", " : "", va_arg(args, int));
-                printed = 1;
-                break;
-            case 'i':
-                printf("%s%d", printed ? ", " : "", va_arg(args, int));
-                printed = 1;
-                break;
-            case 'f':
-                printf("%s%f", printed ? ", " : "", (float)va_arg(args, double));
-                printed = 1;
-                break;
-            case 's':
-                str = va_arg(args, char *);
-                if (!str)
-                    str = "(nil)";
-                printf("%s%s", printed ? ", " : "", str);
-                printed = 1;
-                break;
-        }
-        i++;
-    }
+while (format && format[i])
+{
+switch (format[i])
+{
+case 'c':
+printf("%s%c", printed ? ", " : "", va_arg(args, int));
+printed = 1;
+break;
+case 'i':
+printf("%s%d", printed ? ", " : "", va_arg(args, int));
+printed = 1;
+break;
+case 'f':
+printf("%s%f", printed ? ", " : "", (float)va_arg(args, double));
+printed = 1;
+break;
+case 's':
+str = va_arg(args, char *);
+if (!str)
+str = "(nil)";
+printf("%s%s", printed ? ", " : "", str);
+printed = 1;
+break;
+}
+i++;
+}
 
-    printf("\n");
-    va_end(args);
+printf("\n");
+va_end(args);
 }
