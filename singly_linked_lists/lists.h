d1c4732829099e5242f6f42a51d4b5193fe4d5d0
eloyarnel <eloyaliceasanchez@gmail.com>
2025-11-25 15:19:00 -0400

    Read me for understand the project

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,4 +1,32 @@
-#ifdef SIZE_T_H
-#define SIZE_T_H
+#ifndef LIST_H
+#define LIST_H
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 size_t print_list(const list_t *h);
-#endif SIZE_T_H
+
+
