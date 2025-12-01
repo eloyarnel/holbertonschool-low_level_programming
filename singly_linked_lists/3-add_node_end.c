3653261f3460f719e98f5ae1c3943673db2b7194
eloyarnel <eloyaliceasanchez@gmail.com>
2025-12-01 10:39:38 -0400

    Project Singly Linked, Task 3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,27 +1,37 @@
 #include "lists.h"
 #include <string.h>
 /**
- * add_node - adds a new node at the beginning of a list_t list
+ * add_node_end - adds a new node at the end of a list_t list
  * @head: pointer to the head of the list
  * @str: string to be added to the new node
- *
  * Return: address of the new element, or NULL if it failed
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 list_t *new_node;
-unsigned int length;
-if (str == NULL)
-return NULL;
+list_t *temp;
+unsigned int len;
 new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
-return NULL;
-length = 0;
-while (str[length] != '\0')
-length++;
+return (NULL);
 new_node->str = strdup(str);
-new_node->len = length;
-new_node->next = *head;
+if (new_node->str == NULL)
+{
+free(new_node);
+return (NULL);
+}
+for (len = 0; str[len]; len++)
+;
+new_node->len = len;
+new_node->next = NULL;
+if (*head == NULL)
+{
 *head = new_node;
-return new_node;
+return (new_node);
+}
+temp = *head;
+while (temp->next != NULL)
+temp = temp->next;
+temp->next = new_node;
+return (new_node);
 }
