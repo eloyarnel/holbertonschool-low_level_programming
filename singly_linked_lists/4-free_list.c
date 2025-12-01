f9a4288d1befef6fc78d12115750aae3b2411834
eloyarnel <eloyaliceasanchez@gmail.com>
2025-12-01 10:56:25 -0400

    Project Singly Linked, Task 4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the head of the list
+ */
+void free_list(list_t *head)
+{
+list_t *temp;
+while (head != NULL)
+{
+temp = head;
+head = head->next;
+free(temp->str);
+free(temp);
+}
+}
