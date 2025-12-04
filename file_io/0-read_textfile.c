c6794ce960156004440d7d4746892536eda3a664
eloyarnel <eloyaliceasanchez@gmail.com>
2025-12-04 16:13:38 -0400

    Project File I/O, task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,22 +1,25 @@
 #include "main.h"
-#include <stdlib.h>
+
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
+ * read_textfile - reads a text file and prints it to POSIX stdout
  * @filename: name of the file to read
  * @letters: number of letters it should read and print
  *
- * Return: actual number of letters it could read and print, 0 if it fails
+ * Return: actual number of letters read and printed,
+ *         or 0 on failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t fd, r_bytes, w_bytes;
+int fd;
+ssize_t r_bytes, w_bytes;
 char *buffer;
-if (filename == NULL)
+
+if (filename == NULL || letters == 0)
 return (0);
 fd = open(filename, O_RDONLY);
 if (fd == -1)
 return (0);
-buffer = malloc(sizeof(char) * (letters));
+buffer = malloc(sizeof(char) * letters);
 if (buffer == NULL)
 {
 close(fd);
@@ -29,6 +32,7 @@ free(buffer);
 close(fd);
 return (0);
 }
+
 w_bytes = write(STDOUT_FILENO, buffer, r_bytes);
 if (w_bytes == -1 || w_bytes != r_bytes)
 {
@@ -36,7 +40,9 @@ free(buffer);
 close(fd);
 return (0);
 }
+
 free(buffer);
 close(fd);
+
 return (w_bytes);
 }
