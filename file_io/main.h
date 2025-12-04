9f69e3ba53bd2bd8d1dda3bb5d6c93cf4408a1a5
eloyarnel <eloyaliceasanchez@gmail.com>
2025-12-04 16:19:17 -0400

    Project File I/O, task 0

--- file_io/main.h
+++ file_io/main.h
@@ -1,9 +1,15 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
-#include <sys/types.h>
+
+#include <stddef.h>     /* size_t */
+#include <sys/types.h>  /* ssize_t */
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
-#endif
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif /* MAIN_H */
