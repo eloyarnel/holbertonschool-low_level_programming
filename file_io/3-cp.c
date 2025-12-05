92c73b74bd7c83a2b17bddf215e3ad349214a2e8
eloyarnel <eloyaliceasanchez@gmail.com>
2025-12-05 09:20:09 -0400

    Project File I/O, task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,42 +6,56 @@
  */
 void cp(char *file_from, char *file_to)
 {
-int fd_from, fd_to, r, w;
+int main(int argc, char **argv)
+{
+int fd_from, fd_to;
+ssize_t r_bytes, w_bytes;
 char buffer[1024];
-fd_from = open(file_from, O_RDONLY);
+
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+
+
+fd_from = open(argv[1], O_RDONLY);
 if (fd_from == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-if (fd_to == -1)
+
+fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);if (fd_to == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 close(fd_from);
 exit(99);
 }
-do {
-r = read(fd_from, buffer, 1024);
-if (r == -1)
+
+while ((r_bytes = read(fd_from, buffer, 1024)) > 0)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-close(fd_from);
-close(fd_to);
-exit(98);
-}
-w = write(fd_to, buffer, r);
-if (w == -1)
+w_bytes = write(fd_to, buffer, r_bytes);
+if (w_bytes == -1 || w_bytes != r_bytes)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 close(fd_from);
 close(fd_to);
 exit(99);
 }
-} while (r > 0);
+}
+
+if (r_bytes == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+close(fd_from);
+close(fd_to);
+exit(98);
+}
 if (close(fd_from) == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+close(fd_to);
 exit(100);
 }
 if (close(fd_to) == -1)
@@ -49,19 +63,6 @@ if (close(fd_to) == -1)
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 exit(100);
 }
-}
-/** * main - entry point
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 on success
- */
-int main(int argc, char *argv[])
-{
-if (argc != 3)
-{
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
-}
-cp(argv[1], argv[2]);
+
 return (0);
 }
