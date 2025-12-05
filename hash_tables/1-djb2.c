c0c221b3c43fbd98826440293b57f48fc949f747
eloyarnel <eloyaliceasanchez@gmail.com>
2025-12-05 10:15:05 -0400

    Project Hash Table, task 1

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -5,15 +5,16 @@
  * @str: The string to hash.
  */
 
-unsigned long int hash_djb2(const unsigned char *str);
-
+unsigned long int hash_djb2(const unsigned char *str)
 {
-unsigned long int hash = 5381;
+unsigned long int hash;
 int c;
 
+hash = 5381;
 while ((c = *str++))
 {
 hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 }
+
 return (hash);
 }
