3851519881d1372b399b11defcca73d4a28b70d3
eloyarnel <eloyaliceasanchez@gmail.com>
2025-12-05 12:46:19 -0400

    Project Hash Tables, task 2

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -7,8 +7,10 @@
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 unsigned long int hash;
+
 if (key == NULL || size == 0)
-return(0);
-hash = hash_djb2(key);
-return (hash_djb2 (key) % size);
+return (0);
+
+hash = hash_djb2 (key);
+return (hash % size);
 }
