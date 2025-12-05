891047f33dca200cd9b0f1047f9ad480b20a66cb
eloyarnel <eloyaliceasanchez@gmail.com>
2025-12-05 12:41:52 -0400

    Project Hash Tables, task 2

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+/**
+ * key_index - Gets the index of a key in a hash table array
+ * @key: The key for which to get the index
+ * @size: Size of the array of the hash table
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+unsigned long int hash;
+if (key == NULL || size == 0)
+return(0);
+hash = hash_djb2(key);
+return (hash % size);
+}
