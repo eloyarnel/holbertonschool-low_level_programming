1d08595e6b482032635adcc868b61d483feda01d
eloyarnel <eloyaliceasanchez@gmail.com>
2025-12-05 09:10:05 -0400

    Project File I/O, task 3

--- file_io/main.h
+++ file_io/main.h
@@ -7,7 +7,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>>
+#include <stdio.h>
 #include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
