10853821774fd01d0488d1ff66918e6ace1bb381
eloyarnel <eloyaliceasanchez@gmail.com>
2025-12-15 21:24:33 -0400

    What' my name, task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints its name
+ * @argc: number of arguments
+ * @argv: array of arguments
+ */
+int main(int argc, char *argv[])
+{
+(void)argc; /* Unused parameter */
+printf("%s\n", argv[0]);
+return (0);
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,1 @@
+This Read me is about of the Project of argv for Holberton Coding School.
