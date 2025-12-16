f9ab773a71694a68c9f21c477bb72a8e24ef2aab
eloyarnel <eloyaliceasanchez@gmail.com>
2025-12-15 21:35:44 -0400

    Args, task 1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+
 /**
- * argc_argv - prints all arguments it receives
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0
+ * main - prints the number of arguments passed
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
-int i;
-for (i = 0; i < argc; i++)
-{
-printf("%s\n", argv[i]);
-}
+(void)argv;
+
+printf("%d\n", argc - 1);
 return (0);
 }
+
