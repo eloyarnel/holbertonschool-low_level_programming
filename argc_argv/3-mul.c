245b51f06aecefaaab38acc35eee1a354a4f0c8c
eloyarnel <eloyaliceasanchez@gmail.com>
2025-12-15 21:43:26 -0400

    Args, task 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the result of the multiplication
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
